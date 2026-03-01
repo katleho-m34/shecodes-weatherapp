153a15644269447642a468804e9dd12d0cbba5f6
Katleho <katlehom780@gmail.com>
2026-03-01 12:31:52 +0200

    Imported axios

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@
 
  let searchFormElement = document.querySelector("#search-form");
  searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+ let apiKey = "803006a9ef2843o8c3b4c01a1t50317b";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
+
+ 
